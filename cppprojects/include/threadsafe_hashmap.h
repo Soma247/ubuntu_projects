c45a25c0fb38a92c080b20b198e8d6b1b3dc712f
Soma247 <flammenwerfer36@gmail.com>
2020-07-11 21:00:39 +0300

    upd

---
+++ cppprojects/include/threadsafe_hashmap.h
@@ -0,0 +1,221 @@
+#ifndef THREADSAFE_HASHMAP_H
+#define THREADSAFE_HASHMAP_H
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <forward_list>
+#include <shared_mutex>
+#include <mutex>
+namespace ts_adv{
+
+   template<typename Key, typename T,
+      typename Hash=std::hash<Key>,
+      typename Key_eq=std::equal_to<Key>,
+      class Alloc=
+         std::allocator<std::pair<const Key,T>> 
+   >
+   class ts_hashmap{
+   public:
+      using key_type = Key;
+      using mapped_type = T;
+      using key_equal = Key_eq;
+      using hasher = Hash;
+      using allocator_type = Alloc;
+      using value_type = std::pair<const key_type, mapped_type>;
+      using reference = value_type&;
+      using const_reference = const value_type&;
+      using pointer = 
+         typename std::allocator_traits<allocator_type>::pointer;
+      using const_pointer =
+         typename std::allocator_traits<allocator_type>::const_pointer;
+      using size_type = std::size_t;
+      using difference_type = std::ptrdiff_t;
+   private:
+
+      class bucket_type{
+      public:
+         using bucket_value = std::pair<key_type, mapped_type>;
+         using list_type = std::forward_list<value_type,allocator_type>;
+         using iterator = typename list_type::iterator;
+         using const_iterator = typename list_type::const_iterator;
+      private:
+         mutable std::shared_mutex m_mut;
+         list_type m_data;
+
+         [[nodiscard]]
+         iterator find_entry(const key_type& k){
+            return std::find_if(std::begin(m_data),std::end(m_data),
+                     [&k](const bucket_value& v)->bool{
+                        return key_equal(v.first, k);
+                     }); 
+         }
+
+         [[nodiscard]]
+         const_iterator find_entry(const key_type& k)const{
+            return std::find_if(std::cbegin(m_data),std::cend(m_data),
+                     [&k](const bucket_value& v)->bool{
+                        return key_equal(v.first, k);
+                     }); 
+         }
+      public:
+         bucket_type(){}
+         bucket_type(const bucket_type&)=delete;
+         bucket_type(bucket_type&& other){
+            std::lock_guard lg{other.m_mut};
+            m_data=std::move(other.m_data);
+         }
+         bucket_type& operator = (const bucket_type&)=delete;
+         bucket_type& operator = (bucket_type&& other){
+            std::scoped_lock sl(m_mut, other.m_mut);
+            m_data=std::move(other.m_data);
+         }
+         ~bucket_type()=default;
+         
+         template<typename Oit>
+         Oit copy_values(Oit out)const{
+            std::shared_lock sl{m_mut};
+            return std::copy(std::cbegin(m_data),
+                           std::cend(m_data),out);
+         }
+
+         [[nodiscard]]
+         std::optional<mapped_type> value_for(const key_type& k)const{
+            std::shared_lock sl{m_mut};
+            auto cit=find_entry(k);
+            if(cit!=std::cend(m_data))return cit->second;
+         }
+         
+         template<typename M>
+         void insert_or_assign(const key_type& k, M&& m){
+            std::lock_guard lg{m_mut};
+            iterator it=find_entry(k);
+            if(it!=std::end(m_data)){
+               it->second=std::forward<M>(m);
+               return;
+            }
+            m_data.push_front(bucket_value(k,
+                                 std::forward<M>(m)));
+         }
+
+         template<typename M>
+         void insert_or_assign(key_type&& k, M&& m){
+            std::lock_guard lg{m_mut};
+            iterator it=find_entry(k);
+            if(it!=std::end(m_data)){
+               it->second=std::forward<M>(m);
+               return;
+            }
+            m_data.push_front(bucket_value(std::move(k),
+                                 std::forward<M>(m)));
+         }
+         template<typename M>
+         bool insert(const key_type& k, M&& m){
+            std::lock_guard lg{m_mut};
+            iterator it=find_entry(k);
+            if(it==std::end(m_data)){
+               m_data.push_front(bucket_value(k,
+                                 std::forward<M>(m)));
+               return true;
+            }
+            return false;
+         }
+         template<typename M>
+         bool insert(key_type&& k, M&& m){
+            std::lock_guard lg{m_mut};
+            iterator it=find_entry(k);
+            if(it==std::end(m_data)){
+               m_data.push_front(bucket_value(std::move(k),
+                                 std::forward<M>(m)));
+               return true;
+            }
+            return false;
+         }
+
+         size_type remove(const key_type& k){
+            std::lock_guard lg{m_mut};
+            return m_data.remove_if([&k](const bucket_value& val){
+                              return key_equal(val.first,k);
+                              });
+         } 
+         template<typename Pred>
+         size_type remove_if(Pred p){
+            std::lock_guard lg{m_mut};
+            return m_data.remove_if(p);
+         }
+      };//bucket_type
+
+      const size_type m_buckets_count;
+      std::vector<bucket_type> m_buckets;
+      
+      constexpr bucket_type& bucket_for(const key_type& key)const{
+         return m_buckets[hasher(key)];
+      }
+      bucket_type& bucket_for(const key_type& key){
+         return m_buckets[hasher(key)];
+      }
+   public:
+         ts_hashmap(size_type buckets_count):
+            m_buckets_count{buckets_count},
+            m_buckets(m_buckets_count){}
+         
+         size_type buckets_count()const{
+            return m_buckets_count;
+         }
+
+         template<typename Oit>
+         Oit copy(Oit out)const{
+            std::for_each(std::cbegin(m_buckets),std::cend(m_buckets),
+                  [&out](const bucket_type& b){
+                     out=b.copy_values(out);
+                  });
+            return out;
+         }
+
+         template<typename M>
+         void insert_or_assign(const key_type& k, M&& obj){
+            bucket_for(k).insert_or_assign(k,
+                              std::forward<M>(obj));
+         }
+         template<typename M>
+         void insert_or_assign(key_type&& k, M&& obj){
+            bucket_for(k).insert_or_assign(std::move(k),
+                              std::forward<M>(obj));
+         }
+
+         template<typename M>
+         bool insert(const key_type& k, M&& obj){
+            return bucket_for(k).insert(k,
+                              std::forward<M>(obj);
+         }
+
+         template<typename M>
+         bool insert(key_type&& k, M&& obj){
+            return bucket_for(k).insert(std::move(k),
+                                       std::forward<M>(obj);
+
+         }
+
+         std::optional<mapped_type> value_for(const key_type& k)const{
+            return bucket_for(k).value_for(k);
+         }
+
+         size_type remove(const key_type& k){
+            return bucket_for(k).remove(k);
+         }
+
+         template<typename Pred>
+         size_type remove_if(Pred&& p){
+            size_type count{0};
+            std::for_each(std::begin(m_buckets),std::end(m_buckets),
+               [&count,pred=std::forward<Pred>(p)](bucket_type& b){
+                  count+=b.remove_if(pred);
+            });
+            return count;
+         }
+   };
+}//ts_adv
+
+
+
+
+#endif //THREADSAFE_HASHMAP_H
