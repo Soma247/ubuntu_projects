d12978444058b51bde90a80d66fb8bc8bafccbad
Soma247 <flammenwerfer36@gmail.com>
2022-01-22 11:49:38 +0300

    update

--- qtprojects/untitled1/main.cpp
+++ qtprojects/untitled1/main.cpp
@@ -5,9 +5,13 @@ void print(std::string_view sv){
     std::cout<<std::this_thread::get_id()<<std::quoted(sv)<<std::endl;
 
 }
-int main(){
+
+int main(int argc, char* argv[]){
     using namespace std::literals;
     std::thread t(print,"Hello, World!"sv);
+    for(--argc;argc>=0;--argc){
+      std::cout<<argv[argc]<<std::endl;
+    }
     t.join();
     return 0;
 }
